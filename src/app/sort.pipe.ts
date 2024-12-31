18c21206668fec5d26b578bbf99a394862897fdc
Tudor-debug819 <tovidiua@emeal.nttdata.com>
2024-12-31 14:26:31 +0200

    adding temperature symbol to history

--- src/app/sort.pipe.ts
+++ src/app/sort.pipe.ts
@@ -8,7 +8,15 @@ export class SortPipe implements PipeTransform {
 
   transform(value: string[] | number[], direction: 'asc' | 'desc' = 'asc') {
     const sorted = [...value];
-    sorted.sort();
+    sorted.sort((a, b) => {
+      if (direction === 'asc') {
+        return a > b ? 1 : -1;
+      }
+      else{
+        return a > b ? -1 : 1;
+      }
+    });
+    return sorted;
 
   }
 
